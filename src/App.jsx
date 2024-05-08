a6b6db5f47261b9a511d0dab22b5e829d3b67c6b
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-05-08 22:39:45 +0300

    create About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./Components/Header";
 import { Main } from "./Components/Main";
+import { About } from "./Components/About";
 
 function App() {
   return (
     <>
     <Header/>
     <Main/>
+    <About/>
     </>
   );
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,29 @@
+export const About = () => {
+    return(
+        <>
+        <div className="block-about">
+            <h2 className="h2-about">What is Twinschools</h2>
+            <p className="page-about">
+                    <span class="orange">T</span>
+                    <span class="orange">w</span>
+                    <span class="green">i</span>
+                    <span class="green">n</span>
+                    <span class="red">s</span>
+                    <span class="red">c</span>
+                    <span class="blue">h</span>
+                    <span class="blue">o</span>
+                    <span class="blue">o</span>
+                    <span class="orange">l</span>
+                    <span class="orange">s</span> is a project connecting students from different countries for an 8-week program. 
+                    It aims to foster cultural exchange, 
+                    friendships, and collaboration through virtual meetings and activities. 
+                    Participants learn about each other's cultures, develop teamwork skills, 
+                    and build lifelong connections.
+            </p>
+            <div className="presentation">
+
+            </div>
+        </div>
+        </>
+    );
+}
