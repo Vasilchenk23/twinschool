58eadfb209724cce21d5336971d778a48cabbf97
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-06-25 15:02:53 +0300

    fix mobile version

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -73,7 +73,7 @@ export const Contact = () => {
                 </div>
                 <div className="form">
                     <form onSubmit={handleSubmit}>
-                            <p>ANY QUESTIONS LEFT? <br />WRITE US!</p>
+                            <p className='page-contact-question'>ANY QUESTIONS LEFT? <br />WRITE US!</p>
                             <div className="user-info">
                                 <div className="block-first-name">
                                     <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
